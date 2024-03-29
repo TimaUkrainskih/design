e805649117006200b39ad8e7b3a2007113bd1307
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-03-29 23:18:14 +0300

    27.2 Итератор для двухмерного массива int

---
+++ src/main/java/ru/iterator/MatrixIterator.java
@@ -0,0 +1,33 @@
+package ru.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator<Integer> {
+    private final int[][] data;
+    private int row;
+    private int column;
+
+    public MatrixIterator(int[][] data) {
+        this.data = data;
+        this.row = 0;
+        this.column = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (data.length - 1 != row && (data[row].length == 0 || data[row].length == column)) {
+            row++;
+            column = 0;
+        }
+        return column < data[row].length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[row][column++];
+    }
+}
