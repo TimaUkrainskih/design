05cf4ccbf199813f05909ec56a7cadebd638a9b6
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-03-30 20:43:02 +0300

    27.2 Итератор для двухмерного массива int

--- src/main/java/ru/iterator/MatrixIterator.java
+++ src/main/java/ru/iterator/MatrixIterator.java
@@ -20,7 +20,7 @@ public class MatrixIterator implements Iterator<Integer> {
             row++;
             column = 0;
         }
-        return row < data.length && column < data[row].length;
+        return row < data.length;
     }
 
     @Override
