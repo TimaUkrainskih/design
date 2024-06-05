47d6c7c2daa5073e42a9d1aada770576080bb651
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-06-05 22:11:52 +0300

    33.17 Поиск дубликатов

--- src/main/java/ru/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/io/duplicates/DuplicatesVisitor.java
@@ -1,5 +1,6 @@
 package ru.io.duplicates;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
@@ -20,20 +21,24 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty property = new FileProperty(file.toFile().length(), file.getFileName().toString());
-        filesMap.computeIfAbsent(property, set -> new HashSet<>()).add(file);
-        return FileVisitResult.CONTINUE;
+        File fileVisit = file.toFile();
+        if (fileVisit.isFile()) {
+            FileProperty property = new FileProperty(fileVisit.length(), fileVisit.getName());
+            filesMap.computeIfAbsent(property, set -> new HashSet<>()).add(file);
+        }
+        return super.visitFile(file, attrs);
     }
 
     public void findDuplicates() {
-        for (Map.Entry<FileProperty, Set<Path>> entry : filesMap.entrySet()) {
-            Set<Path> paths = entry.getValue();
-            if (paths.size() > 1) {
-                System.out.println(entry.getKey().getName() + " - " + entry.getKey().getSize() + " bytes:");
-                for (Path path : paths) {
-                    System.out.println("\t" + path.toAbsolutePath());
-                }
-            }
-        }
+        filesMap.entrySet().stream()
+                .filter(pair -> pair.getValue().size() > 1)
+                .forEach(pair -> {
+                    FileProperty fileProperty = pair.getKey();
+                    Set<Path> paths = pair.getValue();
+                    System.out.println(fileProperty.getName() + " - " + fileProperty.getSize() + " bytes:");
+                    paths.stream()
+                            .map(Path::toAbsolutePath)
+                            .forEach(path -> System.out.println("\t" + path));
+                });
     }
 }
