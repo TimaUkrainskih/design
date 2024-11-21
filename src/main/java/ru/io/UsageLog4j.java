a5e0ff86145736ce23f51e69d775dcc3a48c902c
Тимофей Никитан <timofejnikitan@h223.net62.bmstu.ru>
2024-11-21 19:28:05 +0300

    36.4 Slf4j - вывод переменных

--- src/main/java/ru/io/UsageLog4j.java
+++ src/main/java/ru/io/UsageLog4j.java
@@ -13,5 +13,16 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        long value0 = 100000000000L;
+        int value1 = 105;
+        short value2 = 35;
+        byte value3 = 40;
+        double value4 = 150.0000012131;
+        float value5 = 134.0042040204204F;
+        boolean value6 = true;
+        char value7 = 'a';
+        LOG.info("Info: long: {}, int: {}, short: {}, byte: {}," +
+                " double: {}, float: {}, boolean: {}, char: {}",
+                value0, value1, value2, value3, value4, value5, value6, value7);
     }
 }
