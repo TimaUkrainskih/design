ef5a610037533ff693720b5df6ca027eb580309d
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2024-12-16 22:24:29 +0300

    37.5 Преобразование JSON в POJO

--- src/main/java/ru/serialization/ExampleObject.java
+++ src/main/java/ru/serialization/ExampleObject.java
@@ -1,15 +1,7 @@
 package ru.serialization;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.Marshaller;
-import jakarta.xml.bind.Unmarshaller;
 import jakarta.xml.bind.annotation.*;
-import org.json.JSONObject;
 
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.Arrays;
 
 @XmlRootElement(name = "exampleObject")
