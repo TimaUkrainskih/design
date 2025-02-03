09735b056351142fa35a2eed55796e15fabbfd2b
Тимофей Никитан <timofejnikitan@MacBook-Pro-Timofej.local>
2025-02-03 20:56:51 +0300

    39.1 Что такое базы данных

--- db/script.sql
+++ db/script.sql
@@ -8,17 +8,17 @@ create table students (
     gender char(1) check (gender in ('F', 'M'))
 );
 
-insert into students (first_name, last_name, birth_date, group_namber, enrollment_date, gender)
+insert into students (first_name, last_name, birth_date, group_number, enrollment_date, gender)
 values
     ('Иван', 'Иванов', '2000-05-10', 'IU8-74', '2019-09-01', 'M'),
     ('Мария', 'Петрова', '2001-02-18', 'IU3-44', '2020-09-01', 'F'),
     ('София', 'Петрова', '1999-01-01', 'IU1-61', '2018-09-01', 'F');
 
 update students
-set group_namber = 'IU8-84'
+set group_number = 'IU8-84'
 where first_name = 'Иван';
 
 delete from students
-where group_namber = 'IU3-44';
+where group_number = 'IU3-44';
 
 select * from students;
